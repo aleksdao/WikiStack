bf6b8d5181568388634c06ba29be3e62ab569f76
Saif Tase <saiftase@gmail.com>
2016-02-25 14:59:37 -0500

    Error handling for development

--- app.js
+++ app.js
@@ -6,21 +6,19 @@ var path = require("path");
 var morgan = require("morgan");
 var bodyParser = require("body-parser")
 var models = require("./models/");
-
-
+     
 var Page = models.Page;
 var User = models.User;
 
 app.set("views", path.join(__dirname + "/views"));
 app.set("view engine", "html");
 app.engine("html", swig.renderFile);
+swig.setDefaults({cache: false});
+
 app.use(bodyParser.urlencoded({ extended: true })); // for HTML form submits
 app.use(bodyParser.json());
-
 app.use(morgan("dev"));
-
 app.use(express.static(path.join(__dirname, '/public')));
-swig.setDefaults({cache: false});
 
 app.use("/wiki", require("./routes/wiki"));
 app.use("/search", require("./routes/search"));
@@ -34,6 +32,11 @@ app.get("/", function(req, res, next) {
 		res.render("index", {pages: pages} )
 	})
 })
+
+app.use(function(err, req, res, next){
+	console.error(err);
+	res.status(500).send(err.message); //For development only
+});
 
 app.listen(process.env.PORT || 1337, function() {
 	console.log("server is running");
